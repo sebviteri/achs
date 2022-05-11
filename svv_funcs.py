e9cdb46900009c2c08fb2e2beddf0981aea42ba4
ssvalenz-uc <ssvalenz-uc@users.noreply.github.com>
2022-05-11 11:55:21 -0400

    feat: added svv_funcs for my main everyday functions

---
+++ svv_funcs.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def limpia_nombre_cols(df):
+    """
+    Limpia nombre de columnas, dejando todo en minúsculas, con guión bajo en vez de espacios, y sin tildes ni caracteres no ASCII
+    """
+    df.columns = df.columns.str.lower().str.strip().str.replace(" ", "_").str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    return df
