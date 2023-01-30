fab00ee5925c8d97a2f5547cb1f96c982b20edd0
ssvalenz-uc <ssvalenz-uc@users.noreply.github.com>
2023-01-30 00:58:32 -0300

    various fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    descripcion_larga = fh.read()
+
 setup(
     name='achsan',
-    version='0.1',
+    version='0.0.1.dev1',
     description='Utilidades para ACHS Analytics',
+    long_description=descripcion_larga, 
+    long_description_content_type="text/markdown", 
     author='Sebastian Viteri',
     author_email='sviteriv@achs.cl',
+    url='https://github.com/sebviteri/achs', 
     packages=find_packages(),
-    install_requires=['pandas'],
-
+    install_requires=['pandas', 'pyspark'],
+    classifiers=[
+        "Development Status :: 1 - Planning", 
+        "Intended Audience :: Data Scientists", 
+        "Programming Language :: Python :: 3", 
+        "Operating System :: Microsoft :: Windows", 
+    ]
 )
